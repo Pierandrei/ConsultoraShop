6671590ca29245023e9cfc6e6e309e657f1509a2
Marcos <marcos.pierandrei@gmail.com>
2016-01-05 16:00:19 -0200

    parte de sweetalert

--- validacao.php
+++ validacao.php
@@ -1,19 +1,19 @@
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="./sweetalert-master/dist/sweetalert.css">
-<script src="./sweetalert-master/dist/sweetalert.min.js"></script>   
 
 <?php
  if (isset($_POST['btconsultora'])) {
-    	?><script>
-swal("Oops...", "login", "error");
-alert("login");
-	</script><?php 
+require_once('./login.html');
+	?>	
+	<script>
+sweetAlert("Oops...", "Campo usuário precisa ter no minimo 5 caracteres", "error");
+
+	</script><?php
 }
 else{
     if (isset($_POST['btconsultoranew'])) {
         	?><script>
 swal("Oops...", "novo user", "error");
-alert("novo user");
+//alert("novo user");
 	</script><?php 
     }
     else {
