e1849ef29ec2a40862e402dc71190c3631236090
RemOpt <RemOpt@192.168.1.214>
2016-01-07 13:54:35 -0200

    teste1

---
+++ menuView.php
@@ -0,0 +1,22 @@
+<meta charset="utf-8">
+
+<?php
+?>
+	<html lang="en">
+
+  
+							<div class="mainmenu pull-left">
+								<ul class="nav navbar-nav collapse navbar-collapse">
+									<li><a href="index.html">Agenda</a></li>
+									<li class="dropdown"><a href="#" class="active">Controle de Estoque<i class="fa fa-angle-down"></i></a>
+										<ul role="menu" class="sub-menu">
+											<li><a href="shop.html" class="active">Gerar Boleto</a></li>
+										</ul>
+									</li> 
+									<li><a href="404.html">Cadastro de lientes</a></li> 
+									<li><a href="404.html">Contas a Receber</a></li>
+									<li><a href="contact-us.html">Contas a Pagar</a></li>
+									<li><a href="contact-us.html">Relatórios</a></li>
+								</ul>
+							</div>						
+	</html>						
