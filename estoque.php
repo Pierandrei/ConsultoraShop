500d95d626def086482aa70cf5440504b7e11847
RemOpt <RemOpt@192.168.1.214>
2016-01-12 15:37:28 -0200

    teste1

--- estoque.php
+++ estoque.php
@@ -98,7 +98,49 @@ include 'session.php';
                     else
                         return false;
                 }
+                                                
             }
+ function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){  
+     var sep = 0;  
+     var key = '';  
+     var i = j = 0;  
+     var len = len2 = 0;  
+     var strCheck = '0123456789';  
+     var aux = aux2 = '';  
+     var whichCode = (window.Event) ? e.which : e.keyCode;  
+     if (whichCode == 13 || whichCode == 8) return true;  
+     key = String.fromCharCode(whichCode); // Valor para o código da Chave  
+     if (strCheck.indexOf(key) == -1) return false; // Chave inválida  
+     len = objTextBox.value.length;  
+     for(i = 0; i < len; i++)  
+         if ((objTextBox.value.charAt(i) != '0') && (objTextBox.value.charAt(i) != SeparadorDecimal)) break;  
+     aux = '';  
+     for(; i < len; i++)  
+         if (strCheck.indexOf(objTextBox.value.charAt(i))!=-1) aux += objTextBox.value.charAt(i);  
+     aux += key;  
+     len = aux.length;  
+     if (len == 0) objTextBox.value = '';  
+     if (len == 1) objTextBox.value = '0'+ SeparadorDecimal + '0' + aux;  
+     if (len == 2) objTextBox.value = '0'+ SeparadorDecimal + aux;  
+     if (len > 2) {  
+         aux2 = '';  
+         for (j = 0, i = len - 3; i >= 0; i--) {  
+             if (j == 3) {  
+                 aux2 += SeparadorMilesimo;  
+                 j = 0;  
+             }  
+             aux2 += aux.charAt(i);  
+             j++;  
+         }  
+         objTextBox.value = '';  
+         len2 = aux2.length;  
+         for (i = len2 - 1; i >= 0; i--)  
+         objTextBox.value += aux2.charAt(i);  
+         objTextBox.value += SeparadorDecimal + aux.substr(len - 2, len);  
+     }  
+     return false;  
+ }  
+            
         </script>
     </head>
 
@@ -145,7 +187,7 @@ include 'session.php';
                                     <div class="col-sm-8">
                                         <p><input type="text" name="codigo" maxlength="10" placeholder="Codigo do Produto" required="required"/></p>
                                         <p><input type="text" name="produto" placeholder="Descrição do Produto" required="required"/></p>
-                                        <p><input type="text" name="preco" maxlength="6" placeholder="Preço do Produto ex:(3.45)" onKeyPress="return NumVirgPonto(event)" required="required"/></p>
+                                        <p><input type="text" name="preco" maxlength="8" placeholder="Preço do Produto ex:(3.45)" onKeyPress="return(MascaraMoeda(this,'.',',',event))" required="required"/></p>
                                         <p><input type="text" name="qtd" maxlength="3" placeholder="Quantidade do Produto" onKeyPress="return Enum(event)" required="required"/></p>
                                     </div>
                                 </div>
