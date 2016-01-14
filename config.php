7652bd1df6dc3c1c0d7a55f04f31dbb8e9812ddc
RemOpt <RemOpt@192.168.1.214>
2016-01-14 16:59:00 -0200

    teste1

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 // PARTE DE CALENDÁRIO
 include './Conection.php';
 // Url donde estara el proyecto, debe terminar con un "/" al final
-$base_url="http://localhost/ConsultoraShop/";
+$base_url="http://localhost/teste-apagardepois/Calendario-Agenda/";
 ?>
